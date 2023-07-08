8e4fff0c6e0ad8f5281cccbb70af4b4fdd28b64e
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-07-08 18:40:29 -0300

    fix: migration for users table

--- db/migrations/20230708155849_create_table_users.ts
+++ db/migrations/20230708155849_create_table_users.ts
@@ -3,6 +3,7 @@ import { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
+    table.uuid('username').unique()
     table.uuid('session_id').index()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
   })
