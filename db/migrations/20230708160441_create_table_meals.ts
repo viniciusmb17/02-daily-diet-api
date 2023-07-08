c6005c36ab3b2226040d4649e89ec54eb510cbec
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-07-08 18:20:08 -0300

    fix: is_on_the_diet field type

--- db/migrations/20230708160441_create_table_meals.ts
+++ db/migrations/20230708160441_create_table_meals.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
     table.uuid('id').primary()
     table.text('name').notNullable()
     table.text('description')
-    table.text('is_on_the_diet')
+    table.boolean('is_on_the_diet')
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
   })
 }
