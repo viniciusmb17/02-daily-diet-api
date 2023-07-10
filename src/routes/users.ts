e702a1a20e99898bd383da0fa7abd8be3f0481b5
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-07-09 22:40:49 -0300

    feat: recuperar métricas de usuario

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
+import { getUserBySessionId } from '../middlewares/get-user-by-session-id'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
@@ -43,4 +44,70 @@ export async function usersRoutes(app: FastifyInstance) {
 
     return reply.status(201).send()
   })
+
+  app.get(
+    '/overview',
+    { preHandler: [checkSessionIdExists, getUserBySessionId] },
+    async (request, reply) => {
+      const { user } = request
+
+      const meals = await knex('meals')
+        .where({
+          user_id: user?.id,
+        })
+        .select()
+
+      const totalMeals = meals.length
+      const totalMealsOnTheDiet = meals.filter(
+        (meal) => meal.is_on_the_diet,
+      ).length
+      const totalMealsNotOnTheDiet = meals.filter(
+        (meal) => !meal.is_on_the_diet,
+      ).length
+
+      const bestSequenceOnTheDiet = getBestSequenceOnTheDiet(meals)
+
+      const overview = {
+        totalMeals,
+        totalMealsOnTheDiet,
+        totalMealsNotOnTheDiet,
+        bestSequenceOnTheDiet,
+      }
+
+      return {
+        user,
+        overview,
+      }
+    },
+  )
+}
+
+type MealsType = {
+  id: string
+  name: string
+  description: string
+  date: string
+  is_on_the_diet: boolean
+  created_at: string
+  user_id: string
+}[]
+
+function getBestSequenceOnTheDiet(meals: MealsType) {
+  const sequences: number[] = []
+  let counter = 0
+
+  if (meals.length === 0) {
+    return 0
+  }
+
+  meals.forEach((meal) => {
+    if (meal.is_on_the_diet) {
+      counter++
+    } else {
+      sequences.push(counter)
+      counter = 0
+    }
+  })
+
+  return Math.max(...sequences)
 }
